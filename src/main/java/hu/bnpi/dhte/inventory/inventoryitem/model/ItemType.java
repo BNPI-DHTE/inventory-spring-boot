5b998edba2e3b939023a7cef642992f2b7c9eeb5
ferenc-attila <attila.ferenc.80@gmail.com>
2022-07-10 20:09:50 +0200

    added low value asset to item types

--- src/main/java/hu/bnpi/dhte/inventory/inventoryitem/model/ItemType.java
+++ src/main/java/hu/bnpi/dhte/inventory/inventoryitem/model/ItemType.java
@@ -2,5 +2,5 @@ package hu.bnpi.dhte.inventory.inventoryitem.model;
 
 public enum ItemType {
 
-    HIGH_VALUE_ASSET, INFRASTRUCTURE, REAL_ESTATE
+    LOW_VALUE_ASSET, HIGH_VALUE_ASSET, INFRASTRUCTURE, REAL_ESTATE
 }
