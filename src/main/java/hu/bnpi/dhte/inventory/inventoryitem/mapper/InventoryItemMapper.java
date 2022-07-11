2b344a091bfd2f03ca865d0be761fc2868f0ffef
ferenc-attila <attila.ferenc.80@gmail.com>
2022-07-11 20:36:43 +0200

    mapstruct mapper interfaces implemented

---
+++ src/main/java/hu/bnpi/dhte/inventory/inventoryitem/mapper/InventoryItemMapper.java
@@ -0,0 +1,7 @@
+package hu.bnpi.dhte.inventory.inventoryitem.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface InventoryItemMapper {
+}
---
+++ src/main/java/hu/bnpi/dhte/inventory/responsible/mapper/ResponsibleMapper.java
@@ -0,0 +1,7 @@
+package hu.bnpi.dhte.inventory.responsible.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ResponsibleMapper {
+}
