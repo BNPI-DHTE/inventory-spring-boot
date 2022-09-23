22ad9f80208c0da1a641f19fae16ebebd11e8d6a
ferenc-attila <attila.ferenc.80@gmail.com>
2022-09-23 15:06:54 +0200

    removed unused imports

--- src/main/java/hu/bnpi/dhte/inventory/InventoryApplication.java
+++ src/main/java/hu/bnpi/dhte/inventory/InventoryApplication.java
@@ -4,7 +4,6 @@ import hu.bnpi.dhte.inventory.files.readers.ExcelReader;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
